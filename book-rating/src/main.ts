40a7ddf545408edf780e14763db054207b5f775f
Ferdinand Malcher <ferdinand@malcher.media>
2022-11-30 10:11:11 +0100

    TypeScript

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -5,3 +5,42 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+///////////////////
+
+export class Customer {
+  /*id: number;
+
+  constructor(id: number) {
+    this.id = id;
+  }*/
+
+  constructor(public id: number) {
+  }
+
+  foobar(foo: string): number {
+    setTimeout(() => {
+      console.log('Die ID ist', this.id);
+    }, 2000);
+
+    return 1;
+  }
+}
+
+const myCustomer = new Customer(3);
+
+console.log(myCustomer.foobar(''));
+
+
+
+setTimeout(function () {
+  console.log('HALLO')
+}, 2000)
+
+export const bar =  arg => { return arg + 1; }
+
+
+
+
+// bar(4)
