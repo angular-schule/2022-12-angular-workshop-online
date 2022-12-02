d54c6e91aac828597d9d578aa941d51d54881069
Ferdinand Malcher <ferdinand@malcher.media>
2022-12-02 14:27:38 +0100

    Routing: Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class BookDetailsComponent {
 
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+    // Synchroner Weg / PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn') // path: 'books/:isbn'
+
+    // Asynchroner Weg / PUSH
+    // TODO: Verschachtelte Subscription vermeiden
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion
+
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+
+    /*
+    TODO:
+    - Buch abrufen per HTTP
+    - anzeigen
+    */
+  }
 }
