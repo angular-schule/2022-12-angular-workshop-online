000ea683230d8fabc574116ee4114a16a6405e30
Ferdinand Malcher <ferdinand@malcher.media>
2022-12-05 11:11:57 +0100

    Vorbereitung Formulare und Suche

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,13 +5,17 @@ import { BooksRoutingModule } from './books-routing.module';
 import { BookComponent } from './book/book.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookSearchComponent } from './book-search/book-search.component';
 
 
 @NgModule({
   declarations: [
     BookComponent,
     DashboardComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    BookCreateComponent,
+    BookSearchComponent
   ],
   imports: [
     CommonModule,
