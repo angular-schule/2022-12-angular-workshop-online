3f4acb987b207ba5eb1eb72948157d177d62c0dc
Ferdinand Malcher <ferdinand@malcher.media>
2022-12-06 11:31:14 +0100

    Observables erstellen

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Subscriber, Observer } from 'rxjs';
 
 @Component({
   selector: 'rxw-creating',
@@ -22,7 +22,48 @@ export class CreatingComponent {
 
     /******************************/
 
-    
+    // of('A', 'B', 'C')
+    // from([1,2,3,4,5,6])
+    // interval(1000) // ---0---1---2---3---4---5 ...
+    // timer(2000) // ------0|
+    // timer(0, 1000) // 0---1---2---3---4---5 ...
+
+    timer(0, 1000).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    });
+
+
+
+    /******************************/
+
+
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+
+      sub.next(5);
+      sub.next(6);
+      sub.next(7);
+
+      setTimeout(() => sub.next(10), 2000)
+      setTimeout(() => sub.next(15), 3000)
+      setTimeout(() => sub.complete(), 4000)
+    }
+
+    const obs: Observer<number> = {
+      next: (value) => console.log(value),
+      error: (err) => console.error(err),
+      complete: () => console.log('Fertig'),
+    }
+
+    // producer(obs);
+
+    // Finnische Notation
+    const myObservable$ = new Observable(producer);
+    // myObservable$.subscribe(obs);
+
+
     /******************************/
   }
 
